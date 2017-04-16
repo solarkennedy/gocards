5320a04c26bc65e14c0af2eb15d44b9b7f942569
Kyle Anderson <kyle@xkyle.com>
2017-04-16 15:15:52 -0700

    gofish primatives

--- games/gofish/gofish.go
+++ games/gofish/gofish.go
@@ -3,77 +3,38 @@ package main
 import (
 	"fmt"
 	"github.com/solarkennedy/gocards/cardlib"
-	"strings"
 )
 
-func War() {
-	fmt.Println("Starting War")
+type GoFishGame struct {
+	deck *cardlib.Deck
+	players int
+	books int
+}
+
+func GoFish() {
 	deck := cardlib.GetDeckOfCards()
 	deck.Shuffle()
 	deck.Print()
 
-	hand_array := deck.DealIntoHands(2, 26)
-	hand1 := hand_array[0]
-	fmt.Println(len(hand1))
-	hand2 := hand_array[1]
-	round_counter := 0
-	winner := 0
+	var game = new(GoFishGame)
+	game.books = 0
+	game.players = 2
+	game.deck = deck
 
-	for len(hand1) > 0 && len(hand2) > 0 {
-		round_counter++
-		fmt.Printf("Round %d... (Player 1: %d cards   Player 2: %d cards)\n", round_counter, len(hand1), len(hand2))
-		hand1, hand2, winner = doRound(hand1, hand2, 0)
-		if len(hand1)+len(hand2) != 52 {
-			panic("BUG: somehow a card got lost!")
-		}
+	for game.books <= 12 {
+		game.DoRound()
 	}
-	fmt.Printf("\nPlayer %d won after %d rounds!\n", winner, round_counter)
+	fmt.Println(game.CalculateWinner())
 }
 
-func cardCompare(card1 string, card2 string) (v int) {
-	n1 := warCardValue(card1)
-	n2 := warCardValue(card2)
-	if n1 == n2 {
-		v = 0
-	} else if n1 > n2 {
-		v = 1
-	} else if n2 > n1 {
-		v = -1
-	}
-	return v
+func (g *GoFishGame) DoRound() {
+	g.books = 13
 }
 
-func CardValue(c string) (v int) {
-	if c == "🂡" || c == "🂱" || c == "🃁" || c == "🃑" {
-		v = 14
-	} else if c == "🃒" || c == "🃂" || c == "🂲" || c == "🂢" {
-		v = 2
-	} else if c == "🂣" || c == "🂳" || c == "🃃" || c == "🃓" {
-		v = 3
-	} else if c == "🃔" || c == "🃄" || c == "🂴" || c == "🂤" {
-		v = 4
-	} else if c == "🂥" || c == "🂵" || c == "🃅" || c == "🃕" {
-		v = 5
-	} else if c == "🃖" || c == "🃆" || c == "🂶" || c == "🂦" {
-		v = 6
-	} else if c == "🂧" || c == "🂷" || c == "🃇" || c == "🃗" {
-		v = 7
-	} else if c == "🃘" || c == "🃈" || c == "🂸" || c == "🂨" {
-		v = 8
-	} else if c == "🂩" || c == "🂹" || c == "🃉" || c == "🃙" {
-		v = 9
-	} else if c == "🂺" || c == "🂪" || c == "🃚" || c == "🃊" {
-		v = 10
-	} else if c == "🂻" || c == "🂫" || c == "🃋" || c == "🃛" {
-		v = 11
-	} else if c == "🂽" || c == "🂭" || c == "🃍" || c == "🃝" {
-		v = 12
-	} else if c == "🂾" || c == "🂮" || c == "🃎" || c == "🃞" {
-		v = 13
-	}
-	return v
+func (g *GoFishGame) CalculateWinner() string {
+	return "I dont know who the winner is yet"
 }
 
 func main() {
-	War()
+	GoFish()
 }
